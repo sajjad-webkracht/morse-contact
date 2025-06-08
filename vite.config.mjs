b2046d9872a529069169ec01dc4946a81896fc49
Sajjad Dashti Nezhad <saggad.dashti@gmail.com>
2025-06-08 23:14:06 +0400

    configuring multipage

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
+    build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        notfound: '404.html',
+        login: 'login.html',
+        morse: 'morse.html',
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ], 
